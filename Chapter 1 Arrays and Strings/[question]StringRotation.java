66531646340a8e4dd932c1a849b6101cf5256bc6
Seokhoon Lee <seokhoonlee9393@gmail.com>
2018-06-12 17:39:36 +0300

    Create [question]StringRotation.java

---
+++ Chapter 1 Arrays and Strings/[question]StringRotation.java
@@ -0,0 +1,17 @@
+class Main {
+    public static class Solution {
+        public static boolean solve(String s1, String s2) {
+            StringBuilder sb = new StringBuilder(s2 + s2);
+            
+            s2 = sb.toString();
+            
+            return s2.indexOf(s1) != -1;
+        }
+    }
+    
+    public static void main(String[] args) {
+        Solution sol = new Solution();
+        
+        System.out.print(sol.solve("waterbottle", "ottlewaterb"));
+    }
+}
