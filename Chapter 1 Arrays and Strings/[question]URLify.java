bfa1af3d359768e34d3a2cea4e8d66b5e448740c
Seokhoon Lee <seokhoonlee9393@gmail.com>
2018-06-14 21:05:01 +0300

    Update [question]URLify.java

--- Chapter 1 Arrays and Strings/[question]URLify.java
+++ Chapter 1 Arrays and Strings/[question]URLify.java
@@ -1,34 +1,49 @@
-// Question 1.3 URLify
-// Solution 1: Replace blank space with %20: O(N), Space: O(1)
+// 1.3 URLify
 
 class Main {
-    public static class Solution1 {
-        public String solve(String inputString, int stringLength) {            
-            char[] charArray = inputString.toCharArray();
-            
-            int j = 0;
-            
-            for (int i = 0; i < stringLength; i++) {
-                if (inputString.charAt(i) == ' ') {
-                    charArray[j] = '%';
-                    charArray[j + 1] = '2';
-                    charArray[j + 2] = '0';
-                    
-                    j += 3;
-                } else {
-                    charArray[j] = inputString.charAt(i);
-                    j++;
+    public static class Solution {
+        public char[] solve(char[] str, int strLen) {   
+            int urlLen = strLen;
+
+            for (int i = 0; i < strLen; i++) {
+                if (str[i] == ' ') {
+                    urlLen += 2;
                 }
             }
             
-            return String.valueOf(charArray);
+            System.out.print(urlLen + "\n");
+
+            int j = urlLen - 1;
+
+            for (int i = strLen - 1; i >= 0; i--) {
+                if (str[i] == ' ') {
+                    str[j] = '0';
+                    str[j - 1] = '2';
+                    str[j - 2] = '%';
+                    j -= 3;
+                } else {
+                    str[j] = str[i];
+                    j--;
+                }
+            }
+
+            return str;
         }
     }
     
     public static void main(String[] args) {
-        Solution1 sol1 = new Solution1();
+        Solution sol = new Solution();
         
-        System.out.print(sol1.solve("apple and banana      ", 16) + "\n");
+        char[] ci = {'a','p','p','l','e',' ','a','n','d',' ','b','a','n','a','n','a',' ',' ',' ',' '};
+        
+        for (int i = 0; i < 16; i++) {
+            System.out.print(ci[i]);
+        }
+        System.out.print("\n");
+        
+        char[] co = sol.solve(ci, 16);
+        
+        for (int i = 0; i < co.length; i++) {
+            System.out.print(co[i]);
+        }
     }
-}
-
